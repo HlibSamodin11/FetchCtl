8bab64fff610ffe60bee833695602e2e960624b5
MstyslavSoroka <mstyslav2010@gmail.com>
2026-03-25 21:00:38 +0000

    some styles, clipboard

--- src/pages/Ascii.jsx
+++ src/pages/Ascii.jsx
@@ -2,14 +2,25 @@ import asciiFile from '../assets/ascii.json';
 
 function Ascii() {
   return (
-    <div className="bg-zinc-900 flex gap-10">
+    <div className="bg-zinc-900 flex gap-10 flex-wrap justify-center items-center">
       {asciiFile.ascii.map((item) => (
-        <pre
-          key={item.id}
-          className="text-zinc-200 text-xs tracking-tighter leading-3 rounded-2xl"
-        >
-          {item.art.join('\n')}
-        </pre>
+        <div>
+          <pre
+            key={item.id}
+            className="text-zinc-200 text-xs tracking-tighter leading-3 rounded-2xl border border-zinc-200 p-20"
+          >
+            {item.art.join('\n')}
+          </pre>
+          <button
+            className="text-zinc-200 bg-zinc-800 border border-zinc-200 px-5 py-2 rounded-2xl"
+            onClick={() => {
+              navigator.clipboard.writeText(item.art.join('\n'));
+              alert('Copied to clipboard');
+            }}
+          >
+            Copy
+          </button>
+        </div>
       ))}
     </div>
   );
