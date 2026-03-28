34a42a354d3e26897c0d9bf8d8908f7889405c23
SimplyNorvin <simplynorvin@gmail.com>
2026-03-28 09:24:27 +0000

    changed images to svg in footer , github svg added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,5 @@
 import { useRef } from 'react';
 
-const DARK_LOGO = "/dark-logo.png";
-const LIGHT_LOGO = "/light-logo.png";
-const DARK_GITHUB = "/dark-github.png";
-const LIGHT_GITHUB = "/light-github.png";
-
 const links = [
   { title: "title1", items: ["text1", "text1", "text1"] },
   { title: "title2", items: ["text2", "text2", "text2"] },
@@ -45,22 +40,15 @@ function Footer() {
 
       <div className="max-w-7xl mx-auto px-6 md:px-12 relative z-10">
         <div className="flex flex-col lg:flex-row justify-between gap-12 lg:gap-24">
-          
+
           {/* brand */}
           <div className="flex-1 max-w-sm">
             <div className="flex items-center gap-3">
-
-              <img
-                src={DARK_LOGO}
-                alt="logo"
-                className="w-7 h-7 rounded-full block dark:hidden"
-              />
-              <img
-                src={LIGHT_LOGO}
-                alt="logo"
-                className="w-7 h-7 rounded-full hidden dark:block"
-              />
-
+              <div className={`rounded-full border`}>
+                <svg className="w-8 h-8">
+                  <use href="/sprite.svg#icon-logo" />
+                </svg>
+              </div>
               <span className="font-bold italic text-lg tracking-tight text-accent-text">
                 FetchCtl
               </span>
@@ -76,14 +64,18 @@ function Footer() {
               rel="noreferrer"
               className="inline-block mt-6 hover:opacity-70 transition"
             >
-              <img
-                src={DARK_GITHUB}
-                className="w-6 h-6 block dark:hidden"
-              />
-              <img
-                src={LIGHT_GITHUB}
-                className="w-6 h-6 hidden dark:block"
-              />
+              <svg
+                className="w-6 h-6 text-accent-text"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M12 0C5.373 0 0 5.373 0 12c0 5.303 3.438 9.8 8.205 11.387.6.111.82-.261.82-.577 0-.285-.011-1.04-.017-2.04-3.338.724-4.042-1.609-4.042-1.609-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.108-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222 0 1.606-.015 2.898-.015 3.293 0 .319.216.694.825.576C20.565 21.796 24 17.3 24 12c0-6.627-5.373-12-12-12z"
+                />
+              </svg>
             </a>
           </div>
 
